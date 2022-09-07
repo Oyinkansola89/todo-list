e1d2246a1680ac8ea758ab9d2bdbdbaaf06bab67
Oyinkansola89 <yinxxs82@gmail.com>
2022-09-07 22:45:02 +0100

    started creating a todo-list

---
+++ index.js
@@ -0,0 +1,4 @@
+// Create a "close" button and append it to each list item
+// Click on a close button to hide the current list item
+// Add a "checked" symbol when clicking on a list item
+// Create a new list item when clicking on the "Add" button
