6883ec321ab4cd10a03a1cb8a683a08e15daaa05
Oyinkansola89 <yinxxs82@gmail.com>
2022-09-25 19:44:41 +0100

    changed the button tag to a span to allow enable the newList function to work

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import addCheckedClass from "./modules/AddCheckedClass.js";
 // Create a "close" button and append it to each list item
+
 // Click on a close button to hide the current list item
 
 const list = document.querySelector("ul");
@@ -7,16 +8,16 @@ list.addEventListener("click", (event) => addCheckedClass(event));
 
 // Create a new list item when clicking on the "Add" button
 //didn't make the newElement function a new module it would look like the below..
-window.createNewList = function createNewList() {
-  const list = document.createElement("li");
+window.newList = function newList() {
+  const li = document.createElement("li");
   const inputValue = document.getElementById("myInput").value;
   let text = document.createTextNode(inputValue);
-  list.appendChild(text);
+  li.appendChild(text);
 
-  if (inputValue === " ") {
+  if (inputValue === "") {
     alert("please type into the search bar what you need to get done");
   } else {
-    document.getElementById("myUL").appendChild(list);
+    document.getElementById("myUL").appendChild(li);
   }
-  document.querySelector("input").value = "";
+  document.querySelector("myInput").value = " ";
 };
